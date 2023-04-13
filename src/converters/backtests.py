576e61abbf88a9b8b7ae023a7b40de109017dc8d
@yurisa2 <yuri@sa2.com.br>
2023-04-13 16:44:13 -0300

    V0.02

--- src/converters/backtests.py
+++ src/converters/backtests.py
@@ -1,6 +1,5 @@
-def convert_duration(duration):
+def convert_backtest_duration(duration):
     duration_split = duration.split(" ")
-
 
     total = int(int(duration_split[0]) * 24) + int(duration_split[2].split(":")[0]) + (
             int(duration_split[2].split(":")[1]) / 60) + (int(duration_split[2].split(":")[2]) / 3600)
