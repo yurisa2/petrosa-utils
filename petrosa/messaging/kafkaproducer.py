32c849e9e7c2099851cac591bc14e59a5e61f8ae
@yurisa2 <yuri@sa2.com.br>
2023-04-28 16:14:15 -0300

    Antonovo sucos

--- petrosa/messaging/kafkaproducer.py
+++ petrosa/messaging/kafkaproducer.py
@@ -5,7 +5,7 @@ from kafka import KafkaProducer
 
 def get_producer():
     producer = KafkaProducer(bootstrap_servers=os.getenv(
-                                 'KAFKA_SUBSCRIBER', 'localhost:9092'),
+        'KAFKA_ADDRESS', 'localhost:9092'),
                              client_id=os.getenv(
                                  "NEW_RELIC_APP_NAME")
                              )
